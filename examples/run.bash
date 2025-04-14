f34c6bd1035ac5da27e4846539c4efcaec9bd525
Miel Peeters <peetersmiel@gmail.com>
2025-04-14 14:29:17 +0200

    feat: update examples/run.bash

--- examples/run.bash
+++ examples/run.bash
@@ -1,10 +1,16 @@
-
 #!/bin/bash
 
-#pip install uv 
-
 cp ../data/header_00.png header.png
-cp ../data/table.png table.png
-uv init
-uv add ..
+cp ../data/table_00.png table.png
+
+if ! command -v uv > /dev/null; then
+    echo "you need to install uv in order to use this script"
+fi
+
+if [ ! -f pyproject.toml ]; then
+    echo "Initializing python uv project"
+    uv init --no-workspace
+    uv add ..
+fi
+
 uv run example.py 
