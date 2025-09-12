ff40b625cc7fdccb430abb636c73da2f07d7a5de
Miel Peeters <peetersmiel@gmail.com>
2025-09-12 16:23:43 +0200

    feat: implement all directions for astar

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,76 @@
+use std::ops::Add;
+
 use numpy::PyReadonlyArray2;
 use pathfinding::prelude::astar as astar_rust;
 use pyo3::prelude::*;
 
 #[derive(FromPyObject, PartialEq, PartialOrd, Eq, Hash, Clone)]
 struct Point(i32, i32);
+
+enum Direction {
+    Right,
+    RightStrict,
+    Down,
+    DownStrict,
+    Left,
+    LeftStrict,
+    Up,
+    UpStrict,
+    Any,
+    Straight,
+    Diagonal,
+}
+
+impl Direction {
+    fn offsets(&self) -> &[Point] {
+        match self {
+            Direction::Right => &[
+                Point(1, -1),
+                Point(1, 0),
+                Point(1, 1),
+                Point(0, -1),
+                Point(0, 1),
+            ],
+            Direction::RightStrict => &[Point(1, -1), Point(1, 0), Point(1, 1)],
+            Direction::Down => &[
+                Point(-1, 1),
+                Point(0, 1),
+                Point(1, 1),
+                Point(-1, 0),
+                Point(1, 0),
+            ],
+            Direction::DownStrict => &[Point(-1, 1), Point(0, 1), Point(1, 1)],
+            Direction::Left => &[
+                Point(-1, -1),
+                Point(-1, 0),
+                Point(-1, 1),
+                Point(0, -1),
+                Point(0, 1),
+            ],
+            Direction::LeftStrict => &[Point(-1, -1), Point(-1, 0), Point(-1, 1)],
+            Direction::Up => &[
+                Point(-1, -1),
+                Point(0, -1),
+                Point(1, -1),
+                Point(-1, 0),
+                Point(1, 0),
+            ],
+            Direction::UpStrict => &[Point(-1, -1), Point(0, -1), Point(1, -1)],
+            Direction::Any => &[
+                Point(-1, -1),
+                Point(0, -1),
+                Point(1, -1),
+                Point(-1, 0),
+                Point(1, 0),
+                Point(-1, 1),
+                Point(0, 1),
+                Point(1, 1),
+            ],
+            Direction::Straight => &[Point(0, -1), Point(-1, 0), Point(1, 0), Point(0, 1)],
+            Direction::Diagonal => &[Point(-1, -1), Point(1, -1), Point(-1, 1), Point(1, 1)],
+        }
+    }
+}
 
 impl Point {
     fn distance(&self, other: &Point) -> u32 {
@@ -35,44 +102,28 @@ impl Point {
             }
         }
 
-        match dir {
-            Direction::Right => vec![
-                Self(x + 1, y),
-                Self(x + 1, y - 1),
-                Self(x + 1, y + 1),
-                Self(x, y - 1),
-                Self(x, y + 1),
-            ],
-            Direction::Down => vec![
-                Self(x - 1, y + 1),
-                Self(x, y + 1),
-                Self(x + 1, y + 1),
-                Self(x + 1, y),
-                Self(x - 1, y),
-            ],
-            Direction::Any => vec![
-                Self(x + 1, y),
-                Self(x + 1, y + 1),
-                Self(x, y + 1),
-                Self(x - 1, y + 1),
-                Self(x - 1, y),
-                Self(x - 1, y - 1),
-                Self(x, y - 1),
-                Self(x + 1, y - 1),
-            ],
-        }
-        .into_iter()
-        .map(|n| {
-            image_cost(img, &n).map(|icost| {
-                let cost = icost + 15 * step_cost(x, y, n.0, n.1, dir);
-                (n, cost)
+        dir.offsets()
+            .iter()
+            .map(|offset| {
+                let n = self + offset;
+                image_cost(img, &n).map(|icost| {
+                    let cost = icost + 15 * step_cost(x, y, n.0, n.1, dir);
+                    (n, cost)
+                })
             })
-        })
-        .collect()
+            .collect()
     }
 
     fn at_goal(&self, goals: &[Point]) -> bool {
         goals.contains(self)
+    }
+}
+
+impl<'a> Add<&'a Point> for &'_ Point {
+    type Output = Point;
+
+    fn add(self, rhs: &'a Point) -> Self::Output {
+        Point(self.0 + rhs.0, self.1 + rhs.1)
     }
 }
 
@@ -82,18 +133,16 @@ impl From<Point> for (i32, i32) {
     }
 }
 
-enum Direction {
-    Right,
-    Down,
-    Any,
-}
-
 impl Direction {
     fn perpendicular(&self, dx: i32, dy: i32) -> bool {
         match self {
-            Direction::Right => dx == 0 && dy != 0,
-            Direction::Down => dy == 0 && dx != 0,
-            Direction::Any => false,
+            Direction::Right | Direction::RightStrict | Direction::Left | Direction::LeftStrict => {
+                dx == 0 && dy != 0
+            }
+            Direction::Down | Direction::DownStrict | Direction::Up | Direction::UpStrict => {
+                dy == 0 && dx != 0
+            }
+            Direction::Any | Direction::Diagonal | Direction::Straight => false,
         }
     }
 }
@@ -106,8 +155,16 @@ impl TryFrom<&str> for Direction {
             "right" => Ok(Self::Right),
             "down" => Ok(Self::Down),
             "any" => Ok(Self::Any),
+            "straight" => Ok(Self::Straight),
+            "diagonal" => Ok(Self::Diagonal),
+            "right_strict" => Ok(Self::RightStrict),
+            "left_strict" => Ok(Self::LeftStrict),
+            "left" => Ok(Self::Left),
+            "up" => Ok(Self::Up),
+            "up_strict" => Ok(Self::UpStrict),
+            "down_strict" => Ok(Self::DownStrict),
             _ => Err(PyErr::new::<pyo3::exceptions::PyValueError, _>(
-                "Direction must be 'right', 'down' or 'any'",
+                "Direction must be 'right', 'down', 'right_strict', 'left_strict', 'up', 'up_strict', 'diagonal', 'straight' or 'any'",
             )),
         }
     }
